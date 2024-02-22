677390ac05636bc1bed25ca5f3bb1a3f8006f52d
SW33TSTUFF <simaakniyaz@gmail.com>
2024-02-22 22:06:52 +0530

    Add round robin algorithm

--- MLQ.py
+++ MLQ.py
@@ -42,14 +42,6 @@ def enqueue(q, p1):
 def dequeue(q):
     if isEmpty(q):
         print("Case! QUEUE IS EMPTY")
-
-
-    # elif (q.front) == q.rear:
-    #     p_return = q.queue_array[q.front]
-    #     q.front = -1
-    #     q.rear = -1
-    #     q.size -= 1
-    #     return p_return
     
     else:
         p_return = q.queue_array[q.front]
@@ -68,6 +60,8 @@ def input_processes():
     noOfProcess = int(input("Enter the number of processes: "))
     q0 = Queue()
     setQueue(q0)
+    q1 = Queue()
+    setQueue(q1)
 
     for i in range(noOfProcess):
         p = Process(0, 0, 0, 0)
@@ -78,8 +72,11 @@ def input_processes():
 
         if p.prio == 0:
             enqueue(q0, p)
+        elif p.prio == 1:
+            enqueue(q1, p)
 
-    return q0
+
+    return q0, q1
 
 def fcfs(q0):
     global globalTime
@@ -108,22 +105,79 @@ def fcfs(q0):
                 quantum_slice = 0
 
 
+def roundrobin(q0):
+    global globalTime
+    quantum_slice = 8
+    robin_slice = 4
+    print("The global time is", globalTime)
+    
+    # Execute the loop at least once
+    while (quantum_slice != 0):
+        initial_size = q0.size
+        initial_front = q0.front
+        robin_slice = 4
+        for i in range(initial_size):
+
+            if (quantum_slice == 0):
+                break
+
+            if (quantum_slice != 0 and robin_slice == 0):
+                break
+
+            diff = abs(quantum_slice - robin_slice)
+
+            if (quantum_slice >= robin_slice):
+                if q1.queue_array[i + initial_front].b_t != 0 and q1.queue_array[i + initial_front].b_t <= robin_slice:
+                    quantum_slice -= q1.queue_array[i + initial_front].b_t
+                    robin_slice -= q1.queue_array[i + initial_front].b_t
+                    globalTime += q1.queue_array[i + initial_front].b_t
+                    q1.queue_array[i + initial_front].b_t = 0
+                    print("Process", q1.queue_array[i + initial_front].p_id, "exited at time", globalTime)
+
+                    dequeue(q1)
+
+                elif q1.queue_array[i + initial_front].b_t != 0 and q1.queue_array[i + initial_front].b_t > robin_slice:
+                    q1.queue_array[i + initial_front].b_t -= robin_slice
+                    globalTime += robin_slice
+                    quantum_slice -= robin_slice
+                    robin_slice = 0
+            
+            elif (robin_slice > quantum_slice):
+                if q1.queue_array[i + initial_front].b_t != 0 and q1.queue_array[i + initial_front].b_t <= quantum_slice:
+                    quantum_slice -= q1.queue_array[i + initial_front].b_t
+                    robin_slice -= q1.queue_array[i + initial_front].b_t
+                    globalTime += q1.queue_array[i + initial_front].b_t
+                    q1.queue_array[i + initial_front].b_t = 0
+                    print("Process", q1.queue_array[i + initial_front].p_id, "exited at time", globalTime)
+
+                    dequeue(q1)
+                elif q1.queue_array[i + initial_front].b_t != 0 and q1.queue_array[i + initial_front].b_t > quantum_slice:
+                    q1.queue_array[i + initial_front].b_t -= quantum_slice
+                    # Dont need to account for robin_slice from this point 
+                    globalTime += quantum_slice
+                    quantum_slice = 0
+
+            # we need 2 conditions, we need the absolute value of rr slice and quantum slice
+
+
 def schedule():
     global globalTime
     no_queues = 2
     tick = 0
-    quantum_slice = 2
+    quantum_slice = 8
     switch_count = 0
 
     while (globalTime < 25):
         if (switch_count == 0):
-            fcfs(q0)
+            # fcfs(q0)
+            roundrobin(q1)
         elif (switch_count == 1):
             globalTime += quantum_slice
         switch_count = (switch_count + 1) % 2
 
 
 
-q0 = input_processes()
+q0, q1 = input_processes()
 print_queue(q0)
+print_queue(q1)
 schedule()
